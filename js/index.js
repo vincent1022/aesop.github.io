5e561ee541e1d2eb55991e9d6f676d9328f916ba
Vincechenn <a047239442@gmail.com>
2021-02-17 23:13:32 +0800

    [fix] lax.js bug

--- js/index.js
+++ js/index.js
@@ -1,24 +1,35 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
 
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    loop: true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 2800,
-        disableOnInteraction: false,
-    }
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2800,
+    disableOnInteraction: false,
+  },
 });
 
 window.onload = function () {
-    lax.setup() // init
+	lax.init();
 
-    const updateLax = () => {
-        lax.update(window.scrollY)
-        window.requestAnimationFrame(updateLax)
-    }
+	// Add a driver that we use to control our animations
+	lax.addDriver('scrollY', function () {
+		return window.scrollY
+	});
 
-    window.requestAnimationFrame(updateLax)
-}   
+	// Add animation bindings to elements
+	lax.addElements(
+		".parallax_text",
+		{
+			scrollY: {
+				opacity: [
+					[0, 5000],
+					[1, 0]
+				]
+			}
+		},
+		[]
+	);
+}
